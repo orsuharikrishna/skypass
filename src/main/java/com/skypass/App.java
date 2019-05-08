5db2d1c96962f0fc5efd0b3141497dfd6906022a
root <root@ip-172-31-20-178.us-east-2.compute.internal>
2019-05-08 12:02:59 +0000

    maven skypass

---
+++ src/main/java/com/skypass/App.java
@@ -0,0 +1,13 @@
+package com.skypass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
